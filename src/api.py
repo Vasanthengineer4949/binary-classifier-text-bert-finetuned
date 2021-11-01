0adf6f4a593986075e899882aeddf0d99487a573
Vasanth <64586431+Vasanthengineer4949@users.noreply.github.com>
2021-11-01 17:50:54 +0530

    Add files via upload

---
+++ src/api.py
@@ -0,0 +1,60 @@
+from fastapi.applications import FastAPI
+import config
+import torch
+import time
+from model import BERTBaseUncased
+import torch.nn as nn
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+# MODEL = None
+DEVICE = config.DEVICE
+MODEL = BERTBaseUncased()
+MODEL.load_state_dict(torch.load("../model.bin"))
+MODEL.to(DEVICE)
+PREDICTION_DICT = dict()
+
+@app.get("/predict")
+def sentence_prediction(sentence: str):
+    start_time = time.time()
+    tokenizer = config.TOKENIZER
+    max_len = config.MAX_LEN
+    review = str(sentence)
+    review = " ".join(review.split())
+
+    inputs = tokenizer.encode_plus(
+        review, None, add_special_tokens=True, max_length=max_len
+    )
+
+    ids = inputs["input_ids"]
+    mask = inputs["attention_mask"]
+    token_type_ids = inputs["token_type_ids"]
+
+    padding_length = max_len - len(ids)
+    ids = ids + ([0] * padding_length)
+    mask = mask + ([0] * padding_length)
+    token_type_ids = token_type_ids + ([0] * padding_length)
+
+    ids = torch.tensor(ids, dtype=torch.long).unsqueeze(0)
+    mask = torch.tensor(mask, dtype=torch.long).unsqueeze(0)
+    token_type_ids = torch.tensor(token_type_ids, dtype=torch.long).unsqueeze(0)
+
+    ids = ids.to(DEVICE, dtype=torch.long)
+    token_type_ids = token_type_ids.to(DEVICE, dtype=torch.long)
+    mask = mask.to(DEVICE, dtype=torch.long)
+
+    outputs = MODEL(ids=ids, mask=mask, token_type_ids=token_type_ids)
+
+    outputs = torch.sigmoid(outputs).cpu().detach().numpy()
+    positive_prediction = outputs[0][0]
+    negative_prediction = 1 - positive_prediction
+    response = {}
+    response["response"] = {
+        "positive": str(positive_prediction),
+        "negative": str(negative_prediction),
+        "sentence": str(sentence),
+        "time_taken": str(time.time() - start_time),
+    }
+    return response
